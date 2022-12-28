ae4918cb214e945960f8f5f9c735e508d948c7f0
Matej Hladky <mat.hladky@gmail.com>
2022-12-28 22:02:21 +0100

    Initial commit

---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,81 @@
+const client_manifest = {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "file": "entry.5ae2a50b.js",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "dynamicImports": [
+      "virtual:nuxt:/home/mateo/Dev/Projects/mateomd-dev/.nuxt/error-component.mjs"
+    ],
+    "css": [
+      "entry.a244f75e.css"
+    ]
+  },
+  "entry.a244f75e.css": {
+    "file": "entry.a244f75e.css",
+    "resourceType": "style"
+  },
+  "virtual:nuxt:/home/mateo/Dev/Projects/mateomd-dev/.nuxt/error-component.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "file": "error-component.76fc411c.js",
+    "src": "virtual:nuxt:/home/mateo/Dev/Projects/mateomd-dev/.nuxt/error-component.mjs",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "dynamicImports": [
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ]
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "error-404.23ff19d5.js",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "css": []
+  },
+  "error-404.23f2309d.css": {
+    "file": "error-404.23f2309d.css",
+    "resourceType": "style"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "file": "error-500.a5bfba88.js",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "css": []
+  },
+  "error-500.aa16ed4d.css": {
+    "file": "error-500.aa16ed4d.css",
+    "resourceType": "style"
+  },
+  "node_modules/nuxt/dist/app/entry.css": {
+    "resourceType": "style",
+    "file": "entry.a244f75e.css",
+    "src": "node_modules/nuxt/dist/app/entry.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "file": "error-404.23f2309d.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "file": "error-500.aa16ed4d.css",
+    "src": "node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
---
+++ .output/server/index.mjs
@@ -0,0 +1,15 @@
+globalThis._importMeta_={url:import.meta.url,env:process.env};import 'node-fetch-native/polyfill';
+import 'firebase-functions';
+import 'h3';
+export { s as server } from './chunks/nitro/firebase.mjs';
+import 'ofetch';
+import 'destr';
+import 'unenv/runtime/fetch/index';
+import 'hookable';
+import 'scule';
+import 'ohash';
+import 'ufo';
+import 'unstorage';
+import 'defu';
+import 'radix3';
+//# sourceMappingURL=index.mjs.map
