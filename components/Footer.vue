bb75ef8a142432bb90c989234f1fd19fde0f0719
Matej Hladky <mat.hladky@gmail.com>
2022-12-29 15:12:21 +0100

    Footer added

---
+++ components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="row justify-content-center">
+        <p class="small text-muted text-center">© 2022 Mateo</p>
+    </div>
+</template>
+
+<style scoped>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,8 @@
           <Header class="col-10 col-sm-12 mt-1 mb-2" />
           <Navbar class="col-2 col-sm-12" />
           <hr class="d-sm-none">
-          <slot />
+          <slot class="" />
+          <Footer class="pt-5 mt-5 mb-5" />
         </div>
     </div>
 </template>
