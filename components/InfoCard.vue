2140623c7a646015768367bef1c4c918f9e189fe
Matej Hladky <mat.hladky@gmail.com>
2023-01-02 14:24:44 +0100

    Index page showing latest articles and an info card

---
+++ components/InfoCard.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="border-secondary rounded">
+        <h1 class="fs-4">About</h1>
+        <h2 class="fw-bold fs-3 mt-3">Mateo</h2>
+        <p class="text-secondary">I'm a Computer Science student interested primarily in AI, Deep Learning and Computer Vision. I write articles on topics and projects in these areas, as well as technology in general.</p>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,41 @@
 <template>
-  <div>
-    <h1>Latest Article</h1>
+  <div class="row">
+    <div class="col-12 col-lg-8">
+      <h1 class="mb-3">Latest Article</h1>
+      <div v-for="{_path, title, created_at, description} in data" :key="_path">        
+        <NuxtLink :to="_path">
+          <h2 class="fw-bold">{{ title }}</h2>        
+          <p class="text-muted">{{ dateTime(created_at) }}</p>
+          <p>{{ description }}</p>      
+        </NuxtLink>
+      </div>
+      <h1 class="fs-4 mt-5">More Articles</h1>
+      <p>More articles will be published soon!</p> 
+    </div>
+    <div class="col-12 col-lg-4 mt-5 mt-lg-4 ps-lg-5 pe-0">
+        <InfoCard class="ms-lg-5 me-0" />
+    </div>
   </div>
 </template>
 
+<script setup>
+import moment from 'moment';
 
+const { data, refresh } = useAsyncData('index', () => {
+  return queryContent('/articles').limit(1).find();
+})
+
+function dateTime(value) {
+  return moment(value).format('MMMM D, YYYY');
+}
+</script>
+
+<style scoped>
+a {
+	color: var(--foreground-primary-color);
+	text-decoration: none;
+}
+a:hover {
+	color: var(--primary-color);
+}
+</style>
