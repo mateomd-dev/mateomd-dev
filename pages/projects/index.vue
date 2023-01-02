a41cc72367a1d908a661617f86441cd913d39174
Matej Hladky <mat.hladky@gmail.com>
2023-01-02 14:28:29 +0100

    Added github link to the project page

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,6 +1,7 @@
 <template> 
 	<div>
-		<h1 class="text-center display-4 mt-2 mb-5">Projects</h1>
+		<h1 class="text-center display-4 mt-2 mb-3">Projects</h1>
+		<p class="text-center text-secondary mb-5">Visit my <a href="https://github.com/mateomd-dev">GitHub</a> page to see all my projects!</p>
 		<ContentList path="/projects" v-slot="{ list }">     
 				<div v-for="project in list.sort((a, b) => a.id - b.id)" :key="project._path" class="mb-3">
 					<div class="row justify-content-center">
